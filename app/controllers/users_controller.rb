7b9ad6e3223ba06b5de9648a77f80c51f7bf1204
David Cohen <Dovidcohen35@gmail.com>
2020-04-28 18:21:45 -0400

    W7D2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+
+    def show
+        render :show
+    end
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def sign_up
+        user = User.create(:email, :password)
+        redirect_to user_url(:email)
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            redirect_to user_url
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password_digest, :session_token)
+    end
+end
